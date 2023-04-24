66754ca5aa4e766f122bf95da1d3891a93aee90a
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-24 20:01:31 +0100

    Fixed rev

--- print_revstring.c
+++ print_revstring.c
@@ -1,22 +1,23 @@
 #include "main.h"
+
 /**
- * printf_revstr - prints a string
- * @ap: the variable list of arguments
- * Return: the length of the string
+ * printf_revstr - function that prints a str in reverse
+ * @ap: type struct va_arg where is allocated printf arguments
+ *
+ * Return: the string
  */
 int printf_revstr(va_list ap)
 {
-	char *str = va_arg(ap, char *);
-	int i;
-	int length = 0;
 
-	if (str == NULL)
-		str = "(null)";
-	while (str[length] != '\0')
-		length++;
-	for (i = (length - 1); i >= 0; i--)
-	{
-		_putchar(str[i]);
-	}
-	return (length);
+	char *s = va_arg(ap, char*);
+	int i;
+	int j = 0;
+
+	if (s == NULL)
+		s = "(null)";
+	while (s[j] != '\0')
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		_putchar(s[i]);
+	return (j);
 }
