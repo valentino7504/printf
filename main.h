8ae09a437d2f489b417666e103b430ba57d59149
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-24 19:53:50 +0100

    %r specifier

--- main.h
+++ main.h
@@ -20,5 +20,6 @@ int printf_str(va_list ap);
 int printf_37(void);
 int printf_int(va_list ap);
 int printf_b(va_list ap);
+int printf_revstr(va_list ap);
 int _printf(const char *format, ...);
 #endif
---
+++ print_revstring.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * printf_revstr - prints a string
+ * @ap: the variable list of arguments
+ * Return: the length of the string
+ */
+int printf_revstr(va_list ap)
+{
+	char *str = va_arg(ap, char *);
+	int i = 0;
+	int length = 0;
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	length = _strlen(str);
+	for (i = (length - 1); i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	return (length);
+}
