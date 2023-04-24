d6d1b7c66720afb2c93d41b6e6972c09261f70cc
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-24 09:56:40 +0100

    Fixed int and d

--- printf_int.c
+++ printf_int.c
@@ -1,35 +1,44 @@
 #include "main.h"
 /**
- * printf_int - prints an integer to the console
- * @ap: the variable argument list containing the integer to be printed
- * Return: the number of characters printed
+ * printf_int - prints integer
+ * @ap: argument to print
+ * Return: number of characters printed
  */
 int printf_int(va_list ap)
 {
-	int n = va_arg(ap, int), count = 0, digit, exp = 1;
-	int n_copy = n;
+	int n = va_arg(ap, int);
+	int num, last = n % 10, digit, exp = 1;
+	int  i = 1;
 
-	if (n_copy == 0)
-	{
-		_putchar('\0');
-		return (1);
-	}
-	if (n_copy < 0)
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
 	{
 		_putchar('-');
-		n_copy = -n_copy;
-		count++;
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
 	}
-	while (n_copy/exp)
-		exp *= 10;
-	exp /= 10;
-	while (exp)
+	if (num > 0)
 	{
-		digit = n_copy / exp;
-		_putchar(digit + '0');
-		n_copy -= (digit * exp);
-		exp /= 10;
-		count++;
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
 	}
-	return (count);
+	_putchar(last + '0');
+
+	return (i);
 }
