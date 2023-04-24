b2783df46acfa30e8bce75d993ef0df4a86487a6
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-24 19:58:31 +0100

    Rev string fix

--- print_revstring.c
+++ print_revstring.c
@@ -7,14 +7,12 @@
 int printf_revstr(va_list ap)
 {
 	char *str = va_arg(ap, char *);
-	int i = 0;
+	int i;
 	int length = 0;
 
 	if (str == NULL)
-	{
 		str = "(null)";
-	}
-	while (str[length])
+	while (str[length] != '\0')
 		length++;
 	for (i = (length - 1); i >= 0; i--)
 	{
