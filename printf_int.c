9787a6382076a600ae2b9241a0767d3c5df176e0
Edwin Ade <edwinmbonyjr@gmail.com>
2023-04-24 11:15:04 +0100

    Fixed int finally

--- printf_int.c
+++ printf_int.c
@@ -1,38 +1,44 @@
 #include "main.h"
 /**
- * printf_int - prints an integer to the console
- * @ap: the variable argument list containing the integer to be printed
- *
- * Return: the number of characters printed
+ * printf_int - prints integer
+ * @ap: argument to print
+ * Return: number of characters printed
  */
 int printf_int(va_list ap)
 {
-	int n = va_arg(ap, int), temp;
-	int count = 0;
-	int divisor = 1;
+	int n = va_arg(ap, int);
+	int copy, final = n % 10, divisor = 1;
+	int count = 1;
 
 	if (n == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
+	n /= 10;
+	copy = n;
 	if (n < 0)
 	{
-		_putchar('-');
+		copy = -copy;
 		n = -n;
+		final = -final;
 		count++;
+		_putchar('-');
 	}
-	temp = n;
-	while (temp > 9)
+	while (copy / 10)
 	{
-		divisor *= 10;
-		temp /= 10;
+		divisor = divisor * 10;
+		copy /= 10;
 	}
-	while (divisor >= 1)
+	copy = n;
+	while (divisor > 0)
 	{
-		_putchar((n / divisor) + '0');
+		_putchar((copy / divisor) + '0');
+		copy -= ((copy / divisor) * divisor);
 		divisor /= 10;
 		count++;
 	}
+	_putchar(final + '0');
+
 	return (count);
 }
